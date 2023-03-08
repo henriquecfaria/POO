1e5f97571c3857262894555d1b8949b05e3ceaf2
henriquecfaria <125416567+henriquecfaria@users.noreply.github.com>
2023-03-07 22:23:17 -0300

    Add files via upload

---
+++ contabancaria/src/exercicio3notas/main.java
@@ -0,0 +1,9 @@
+package exercicio3notas;
+
+public class main {
+    public static void main(String[] args) {
+        Aluno aluno1 = new Aluno("joão","123",20,30,"ADS",3);
+         aluno1.avaliarAluno();
+        aluno1.avaliarRecuperacao(80);
+    }
+}
